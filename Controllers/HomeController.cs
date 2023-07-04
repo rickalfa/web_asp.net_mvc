0686466894f68b1e5bb155b484465d8e7f6933b6
ricardo betancourt <rb__samael_@hotmail.es>
2023-07-03 22:48:44 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Modelos3d()
+        {
+            ViewBag.Message = "vista de diseños 3D";
+
+            return View();
+        }
+
+
+
+    }
+}
